5697e777c0704679efaa433af9616ae0aa278f7f
xgdrepo <18790125026@163.com>
2026-01-27 15:23:55 +0800

    修改build.sh，解决了编译报错问题

--- 仿真/ws/src/livox_ros_driver2/build.sh
+++ 仿真/ws/src/livox_ros_driver2/build.sh
@@ -25,8 +25,6 @@ else
 fi
 echo "ROS version is: "$ROS_VERSION
 
-
-
 # exit
 
 # substitute the files/folders: CMakeList.txt, package.xml(s)
@@ -47,7 +45,29 @@ fi
 pushd `pwd` > /dev/null
 if [ $ROS_VERSION = ${VERSION_ROS1} ]; then
     cd ../../
-    catkin_make -DROS_EDITION=${VERSION_ROS1}
+    
+    # ========== 修改开始：分步编译解决依赖问题 ==========
+    echo "步骤 1/3: 生成 livox_ros_driver2 的消息文件..."
+    catkin_make -DROS_EDITION=${VERSION_ROS1} livox_ros_driver2_generate_messages
+    
+    echo "步骤 2/3: 编译 livox_ros_driver2 驱动节点..."
+    catkin_make -DROS_EDITION=${VERSION_ROS1} livox_ros_driver2_node
+    
+    echo "步骤 3/3: 编译所有包..."
+    catkin_make -DROS_EDITION=${VERSION_ROS1} -j4
+    
+    # 或者使用下面这个更全面的方法：
+    # echo "步骤 1/4: 生成所有包的消息文件..."
+    # catkin_make -DROS_EDITION=${VERSION_ROS1} generate_messages
+    
+    # echo "步骤 2/4: 单独编译 livox_ros_driver2..."
+    # catkin_make -DROS_EDITION=${VERSION_ROS1} --only-pkg-with-deps livox_ros_driver2
+    
+    # echo "步骤 3/4: 编译其他包..."
+    # catkin_make -DROS_EDITION=${VERSION_ROS1} --start-with livox_ros_driver2 --continue-on-failure -j4
+    
+    # ========== 修改结束 ==========
+    
 elif [ $ROS_VERSION = ${VERSION_ROS2} ]; then
     cd ../../
     colcon build --cmake-args -DROS_EDITION=${VERSION_ROS2} -DHUMBLE_ROS=${ROS_HUMBLE}
