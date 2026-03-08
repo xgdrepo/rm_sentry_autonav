38a606e94b16833f4ae988881d51793f25578e57
xgdrepo <18790125026@163.com>
2026-03-08 21:04:54 +0800

    删除多余文件

--- 仿真/ws/build/robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xu/ws/src/robot
-# Build directory: /home/xu/ws/build/robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 仿真/ws/build/serial_com/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xu/ws/src/serial_com
-# Build directory: /home/xu/ws/build/serial_com
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
